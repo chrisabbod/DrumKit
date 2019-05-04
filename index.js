75473de72233e06b087ed437444a406f297ff94c
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2019-05-04 18:27:58 -0400

    Add animation to buttons when pressed

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-debugger;
-
 var numberOfDrumButtons = document.querySelectorAll(".drum").length
 
+//Detect button press
 for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
         var buttonInnerHTML = this.innerHTML;
         
         makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
+//Detect keyboard press
 document.addEventListener("keydown", function (event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 })
 
 function makeSound(key) {
@@ -49,4 +52,12 @@ function makeSound(key) {
             console.log(buttonInnerHTML);
             break;
     }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
